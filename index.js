d884fa11617b30a649c976ee8b24c12c7da1db95
Miroj <zorim.rai@gmail.com>
2022-05-17 14:17:58 +0545

    first-commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+import usersRoutes from './routes/users.js'
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use('/users', usersRoutes);
+
+app.get('/', (req, res)=>{ res.send('Homepage');});
+app.listen(PORT, ()=> console.log(`Server Running on port: http://localhost:${PORT}`));
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+import express from 'express';
+const router = express.Router();
+import { createUser, getUsers, getUser, deleteUser, updateUser } from '../controllers/users.js'
+
+router.get('/', getUsers);
+
+router.post('/', createUser);
+
+router.get('/', getUser);
+
+router.delete('/:id', deleteUser);
+
+router.patch('/:id', updateUser);
+
+export default router;
